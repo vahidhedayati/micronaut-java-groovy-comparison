a84ba83cddc4b4a518b7c6c3721a7277e321bd5e
Vahid Hedayati <badvad@gmail.com>
2018-11-16 09:57:27 +0000

    Flowable code syntax issues with groovy -

--- test-groovy/src/main/groovy/comparison.java.groovy/controller/OrdersRepository.groovy
+++ test-groovy/src/main/groovy/comparison.java.groovy/controller/OrdersRepository.groovy
@@ -2,9 +2,12 @@ package comparison.java.groovy.controller
 
 import comparison.java.groovy.client.ItemClient
 import comparison.java.groovy.domain.Orders
+import comparison.java.groovy.view.Item
 import io.lettuce.core.KeyValue
 import io.lettuce.core.api.StatefulRedisConnection
 import io.lettuce.core.api.reactive.RedisReactiveCommands
+import io.micronaut.core.convert.value.ConvertibleValues
+import io.reactivex.Flowable
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
@@ -53,47 +56,49 @@ class OrdersRepository {
         return Mono.from(itemClient.find(
                 slug
         ).toFlowable())
-                .flatMap( {petInstance ->
-                ZonedDateTime expiryDate = ZonedDateTime.now().plus(duration);
-                Orders  order = new Orders(petInstance, description, price);
-                Map<String, String> data = dataOf(price, description, order.getCurrency());
+                .flatMap({ petInstance ->
+            ZonedDateTime expiryDate = ZonedDateTime.now().plus(duration);
+            Orders order = new Orders(petInstance, description, price);
+            Map<String, String> data = dataOf(price, description, order.getCurrency());
 
-                String key = petInstance.getName();
-                RedisReactiveCommands<String, String> redisApi = redisConnection.reactive();
-        return redisApi.hmset(key,data)
-                .flatMap({success-> redisApi.expireat(key, expiryDate.toEpochSecond() )})
-                .map({ok -> order}) ;
-    });
+            String key = petInstance.getName();
+            RedisReactiveCommands<String, String> redisApi = redisConnection.reactive();
+            return redisApi.hmset(key, data)
+                    .flatMap({ success -> redisApi.expireat(key, expiryDate.toEpochSecond()) })
+                    .map({ ok -> order });
+        });
     }
 
     private Map<String, String> dataOf(BigDecimal price, String description, Currency currency) {
-        Map data=[:]
-        data.currency=currency.getCurrencyCode()
-        data.price=price as String
-        data.description=description
+        Map data = [:]
+        data.currency = currency.getCurrencyCode()
+        data.price = price as String
+        data.description = description
         return data;
     }
 
     /**
-     * Broken conversion java groovy
+     * Broken conversion java groovy / now working but not sure if it is what is supposed to be and do :)
      * @param commands
      * @return
      */
     private Function<String, Mono<? extends Orders>> keyToOrder(RedisReactiveCommands<String, String> commands) {
-        return key {-> {
-            Flux<KeyValue<String, String>> values = commands.hmget(key, "price", "description");
+        Flux<KeyValue<String, String>> values = commands.hmget(key, "price", "description");
             Map<String, String> map = new HashMap<>(3);
-            return values.reduce(map, (all, keyValue) -> { all.put(keyValue.getKey(), keyValue.getValue());
-            return all;
-        })
-            .map(ConvertibleValues::of)
-            .flatMap({entries -> {
-            String description = entries.get("description", String.class).orElseThrow(() -> new IllegalStateException("No description"));
-            BigDecimal price = entries.get("price", BigDecimal.class).orElseThrow(() -> new IllegalStateException("No price"));
-            Flowable<Item> findItemFlowable = itemClient.find(key).toFlowable();
-            return Mono.from(findItemFlowable).map(item -> new Orders(item, description, price));
-        }});
-        };}
+            values.reduce({all, keyValue->
+                all.put(keyValue.getKey(), keyValue.getValue());
+                return all
+        }).map({entries -> ConvertibleValues.of(entries)})
+                    .flatMap({entries ->  bindEntry(entries)});
+        return values.key
+    }
+
+    private Mono<Orders> bindEntry(entries) {
+        String description = entries.get("description", String.class).orElseThrow({ new IllegalStateException("No description")});
+        BigDecimal price = entries.get("price", BigDecimal.class).orElseThrow({new IllegalStateException("No price")});
+        Flowable<Item> findItemFlowable = itemClient.find(key).toFlowable();
+        return Mono.from(findItemFlowable).map({item -> new Orders(item, description, price)});
+
     }
 
 }
